f048ce0c2dd3559ec1ff092e8a226bd6e9be6727
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-04 10:43:13 +0530

    ForkAndPullRequest class added

---
+++ AutomationFramework_10/src/test/java/com/qspiders/bhim/ForkAndPullRequest.java
@@ -0,0 +1,11 @@
+package com.qspiders.bhim;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForkAndPullRequest {
+	@Test
+	public void practise() {
+		Reporter.log("ForkAndPullRequest class added",true);
+	}
+}
